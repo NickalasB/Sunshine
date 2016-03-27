7c8aae46fb76d92c26bb5aad98a565e2f35566f3
Nick Bradshaw <nickalasb@gmail.com>
2016-03-27 05:56:12 -0700

    This is my commit for node 3.04

---
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -0,0 +1,49 @@
+package com.example.android.sunshine.app;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nickbradshaw on 3/26/16.
+ */
+public class DetailFragment extends Fragment {
+
+    @Override public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //ensures that a menu is happening in this activity
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.detail, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //Getting the extra text form the forecast defined in ForecastFragment
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        return rootView;
+    }
+}
+
+
+
+
