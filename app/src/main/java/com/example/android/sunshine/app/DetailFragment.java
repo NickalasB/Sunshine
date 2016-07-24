0bbbe2e36dcec819b545be8ecab30b77af93dd60
Nick Bradshaw <nickalasb@gmail.com>
2016-07-24 09:43:55 -0700

    6.21 non working shared elements. Trying to see what changes with the working version

--- app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -33,6 +33,8 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
 
     private static final String LOG_TAG = DetailFragment.class.getSimpleName();
     static final String DETAIL_URI = "URI";
+    static final String DETAIL_TRANSITION_ANIMATION = "DTA";
+
 
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
@@ -40,6 +42,7 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
     private ShareActionProvider mShareActionProvider;
     private String mForecast;
     private Uri mUri;
+    private boolean mTransitionAnimation;
 
     public static final int DETAIL_LOADER = 0;
 
@@ -102,6 +105,8 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
         Bundle arguments = getArguments();
         if (arguments != null) {
             mUri = arguments.getParcelable(DetailFragment.DETAIL_URI);
+            mTransitionAnimation = arguments.getBoolean(DETAIL_TRANSITION_ANIMATION, true);
+
         }
         View rootView = inflater.inflate(R.layout.fragment_detail_start, container, false);
 
@@ -266,18 +271,15 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
             // We still need this for the share intent
             mForecast = String.format("%s - %s - %s/%s", dateText, description, high, low);
 
-//            // If onCreateOptionsMenu has already happened, we need to update the share intent now.
-//            if (mShareActionProvider != null) {
-//                mShareActionProvider.setShareIntent(createShareForecastIntent());
-//            }
         }
 
 
         AppCompatActivity activity = (AppCompatActivity)getActivity();
         Toolbar toolbarView = (Toolbar) getView().findViewById(R.id.toolbar);
 
+
         // We need to start the enter transition after the data has loaded
-        if (activity instanceof DetailActivity) {
+        if (mTransitionAnimation) {
             activity.supportStartPostponedEnterTransition();
 
             if ( null != toolbarView ) {
